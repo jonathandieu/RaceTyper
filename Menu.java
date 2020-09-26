87ba9b419875b06158ced9e8079c31839a3533a5
jonathanhuydieu <jonathanhuydieu@knights.ucf.edu>
2020-09-26 19:41:14 -0400

    still a work in progress

--- Menu.java
+++ Menu.java
@@ -1,89 +1,174 @@
-import java.util.*
+import java.io.FileInputStream;
+import java.util.*;
 
 public class Menu
 {
 
-  private ArrayList<LinkedList<String>> blockLoader;
+  // Menu variables
+  private ParagraphManager paraManager;
   private Scanner sc;
   private int timeLimit = 20;
-
-  // Implement this functionality later
-  private String file;
+  private String filename;
   
+
+  // Constructor to initialize the scanner and the paraManager
   public Menu()
   {
     sc = new Scanner(System.in);
-    blockLoader = new ArrayList<>();
-  
-    
+    paraManager = new ParagraphManager(sc);
   } 
 
+  // Take in input from a text file with paragraph in it as specified by the user
+  // public String fileReader() 
+  // {
+  //   System.out.println("Please enter the name of the file that you would like to use as input?");
+    
+  //   filename = sc.nextLine();
+    
+    // Takes file specified by the user and
+    // Scanner inputscanner = new Scanner(new File(filename)); 
+    // while (inputscanner.hasNextLine())
+    // {
+    //   String line = inputscanner.nextLine();
+    // }
+
+    
+      
+ // }
+  // Wasn't working for some reason
+    /*
+  { FileInputStream fstream = new FileInputStream("");
+    File inputfile = new File("input.txt");
+
+    try (BufferedReader br = new BufferedReader(new FileReader(inputfile)));
+      for(string line; (line = br.readLine()) != null; )
+    {
+      System.out.println(line);
+    }
+    
+    fstream.close();
+  }
+  */
+  
+  // Initial options menu after a paragraph is created or chosen.
   public void splashOptions()
   {
-    System.out.println("Press 1 to start the game.");
-    System.out.println("Press 2 to die");
-    System.out.println("Press 3 to exit the game");
+    createHeader("1: Start a game.\n2: Paragraph editor.\n3: Exit game");
     
     int choice = sc.nextInt();
     switch(choice)
     {
       case 1:
-      start();
-      break;
+        initGame();
+        break;
       
       case 2:
-      break;
+        paraManager.paragraphMenu();
+        break;
 
       case 3:
-      
-      return;
-      System.out.println("Thanks for playing!\n");
-      System.exit(0);
+        System.out.println("Thanks for playing!\n");
+        System.exit(0);
     }
   }   
   
-  
+  // Function that asks the user to choose a paragraph they wish to practice, displays titles,
+  // and they choose the index assosciated with that specific paragraph. Starts the game.
+  public void initGame() 
+  {
+    System.out.println("Please choose a paragraph to practice:");
+    paraManager.titleList();
+    int pgraph_num = sc.nextInt();
 
+    System.out.println("Please choose a difficulty (1-5):");
+    int difficulty = sc.nextInt();
+
+    startGame(pgraph_num, difficulty);
+  }
+
+  // Function to output a list of all titles along with the index asssociated with that specific paragraph.
+  // public void titleList()
+  // {
+  //   for (int i = 0; i < paraManager.size(); i++) 
+  //   {
+  //     System.out.println(i + " " + "|" + " " + paraManager.get(i).getTitle());
+  //   }
+  //   System.out.println();
+  // }
+
+
+  // Initial start menu prompting the user if they would like to create a new paragaph or use
+  // an existing one(implemented later).
   public void startMenu() {
-    System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-==-=-=-=-=-=-=-=-=-");
-    System.out.println("Welcome to RacerType!");
-    System.out.println("Created by Jonathan Dieu, John Pham, and Ryan Black");
-    System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-==-=-=-=-=-=-=-=-=-");
+    createHeader("Welcome to RacerType!\nCreated by Jonathan Dieu, John Pham, and Ryan Black");
 
     System.out.println("Would you like to create a new paragraph to practice on? Y/N");
-    char res = sc.next();
+    String res = sc.next();
   
-    if (res == 'Y')
-        continue;
-        
-    else
-     exit(0);
-
-    System.out.println("Please enter your paragraph:");
-    enterParagraph();
+    if (res != "Y")
+       splashOptions();
     
-  } 
- 
- public void enterParagraph() {
-  String input;
-  paragraph = new Paragraph();
-
-  while (input != "@@") {
-      input = sc.next();
-      paragraph.add(input);
+    paraManager.enterParagraph();
+    splashOptions();
+    
   }
-  blockLoader.add(paragraph);
- }
 
- public void addToParagraph(int paragraph)
- {
-   while (true)
-   {
-     System.out.println("Enter in a sentence to add");
-     
-   }
+  // // Function that creates our beautiful header.
+  public void createHeader(String header)
+  {
+    System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-==-=-=-=-=-=-=-=-=-");
+    System.out.println(header);
+    System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-==-=-=-=-=-=-=-=-=-\n\n");
+  }
+  // // The paragraph editor menu :D
+  // public void paragraphEditorMenu()
+  // {
+  //   createHeader("Paragraph Editor\nHere's what you've saved so far:");
+  //   titleList(); 
+  //   createHeader("1: Start a game.\n2: Paragraph editor.\n3: Exit game")
+  // }
+  
+  
+ 
+//  public void enterParagraph()
+//   {
+//     String input;
+//     Paragraph paragraph = new Paragraph();
 
- }
+//     System.out.println("Please enter a title for your paragraph:");
+//     paragraph.setTitle(sc.next);
+
+//     System.out.println("NOTE: Enter @@ when you finish typing your paragraph!");
+//     System.out.println("Please enter your paragraph:");
+  
+//     while (input != "@@") 
+//     {
+//         input = sc.next();
+//         paragraph.add(input);
+//     }
+  
+//     blockLoader.add(paragraph);
+//  }
+
+//  public void addToParagraph(int paragraph)
+//  {
+//    while (true)
+//    {
+//      System.out.println("Enter in a sentence to add; @@ to leave");
+//      String input = sc.nextLine();
+
+//      if (input.isEmpty())
+//      {
+//        continue;
+//      }
+//      if (input.equals("@@"))
+//      {
+//        return;
+//      }
+//      blockLoader.get(paragraph).add(input);     
+//    }
+
+//  }
   // Method that prints out your options
   // And helper methods for each option
   
@@ -91,15 +176,16 @@ public class Menu
 
   // Functionality for the game
   // Times users and compares their input to what is being held in blockLoader
-  public void startGame(int paragraph, difficulty)
+  public void startGame(int paragraph, int difficulty)
   {
     int count = 0;
     double overallTime = 0;
     
-    final long start = System.nanoTime();
-    String userInput = sc.nextLine();
-    
-    for (String sentence : blockLoader.get(paragraph))
+    // string = 
+    // System.out.println(string);
+    LinkedList temp = paraManager.get(paragraph).getBlock();
+    System.out.println("")
+    for (int i = 0; i < temp.size(); i++)
     {
       final long start = System.nanoTime();
       String userInput = sc.nextLine();
@@ -107,8 +193,8 @@ public class Menu
 
       double totalTime = (stop - start) / 1e9;
 
-      if (totalTime >= timeLimit * difficulty)
-        typingError(sentence, totalLimit);
+      if (totalTime >= timeLimit * difficulty || !userInput.equals(temp.get(i)))
+        typingError(temp.get(i), userInput, timeLimit);
       
       else if (++count % 3 == 0)
         typingSuccess();
@@ -116,16 +202,17 @@ public class Menu
 
     // IMPLEMENT LATER
     // A report screen that will display some cool stats to the user
-    reportPrint(paragraph, difficulty, overallTime);
+    // reportPrint(paragraph, difficulty, overallTime);
   }
 
   // Occurs when the user can't type a word correctly or on time
-  public void typingError(String sentence, double totalTime)
+  public void typingError(String sentence, String userInput, double totalTime)
   {
-    System.out.println("Oops! Looks like you couldn't finish correctly or on time, let's try this again")
+    System.out.println("Your input: " + userInput + "\nYour time: " + totalTime + " seconds");
+    System.out.println("Oops! Looks like you couldn't finish correctly or on time, let's try this again");
     System.out.println("Try typing this sentence again:\n" + sentence + "\nTake your time, you can do it!");
 
-    String input = Scanner.nextLine();
+    String input = sc.nextLine();
     while (!input.equals(sentence))
     {
       System.out.println("Try again. Take your time, it's okay to slow down a bit");
@@ -137,4 +224,12 @@ public class Menu
   {
     System.out.println("LET'S GOOOOOOOOO!!!\nPOGCHAMP");
   }
+
+  // public static void countdown()
+  // {
+  //   System.out.println("Get ready...");
+  //   {timer = new Timer(); timer.schedule(new DisplayCountdown(), 1000);} // Last parameter is the interval in which the coundown is displayed (frequency wise)
+
+  //  class DisplayCountdown extends TimerTask {int seconds = timeLimit;} // 2 errors
 }
+  
--- Paragraph.java
+++ Paragraph.java
@@ -1,11 +1,49 @@
+import java.util.*;
+
 public class Paragraph
 {
   private LinkedList<String> block;
   private String title;
 
-  public String toString()
+  public Paragraph(String s)
+  {
+    block = new LinkedList<>();
+    setTitle(s);
+  }
+
+  public Paragraph()
+  {
+    block = new LinkedList<String>();
+    title = "RandomTitle";
+  }
+
+  public LinkedList getBlock()
+  {
+    return block;
+  }
+
+  public void add(String sentence)
+  {
+    block.add(sentence);
+  }
+
+  public void setTitle(String s) {
+    title = s;
+  }
+
+  public String getTitle()
   {
     return title;
+  }
+
+  public void printAllSentences()
+  {
+    int i = 1;
+
+    for (String sentence : block)
+    {
+      System.out.println(i++ + ": " + sentence);
+    }
   }
 }
 
